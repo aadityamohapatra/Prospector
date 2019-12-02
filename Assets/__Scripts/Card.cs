064c8ec62a2e9dba1b8198c6c2f0aa45462fb9c2
aadityamohapatra <aaditya.mohapatra@gmail.com>
2019-12-01 19:34:50 -0500

    Prospector Complete
    Updated background, fixed bug where prospector wasn't restarting once the player lost the round which in turn was causing the high score not being set from score from previous round. Future commits will be made to fix minor issues.

--- Assets/__Scripts/Card.cs
+++ Assets/__Scripts/Card.cs
@@ -13,29 +13,80 @@ public class Card : MonoBehaviour {
 	public List<GameObject> pipGOs = new List<GameObject>();
 	
 	public GameObject back;  // back of card;
-	public CardDefinition def;  // from DeckXML.xml		
-
-
-	public bool faceUp {
-		get {
-			return (!back.activeSelf);
-		}
-
-		set {
-			back.SetActive(!value);
-		}
-	}
-
+	public CardDefinition def;  // from DeckXML.xml	
+    public SpriteRenderer[] spriteRenderers;
 
 	// Use this for initialization
 	void Start () {
-	
+        SetSortOrder(0);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    public void PopulateSpriteRenderers()
+    {
+        // If spriteRenderers is null or empty 
+        if (spriteRenderers == null || spriteRenderers.Length == 0)
+        {
+            // Get SpriteRenderer Components of this GameObject and its children 
+            spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+        }
+    }
+
+    public void SetSortingLayerName(string tSLN)
+    {
+        PopulateSpriteRenderers();
+        foreach (SpriteRenderer tSR in spriteRenderers)
+        {
+            tSR.sortingLayerName = tSLN;
+        }
+    }
+    // Sets the sortingOrder of all SpriteRenderer Components 
+    public void SetSortOrder(int sOrd)
+    {                                     // a 
+        PopulateSpriteRenderers();
+        // Iterate through all the spriteRenderers as tSR 
+        foreach (SpriteRenderer tSR in spriteRenderers)
+        {
+            if (tSR.gameObject == this.gameObject)
+            {
+                // If the gameObject is this.gameObject, it's the background 
+                tSR.sortingOrder = sOrd; // Set it's order to sOrd 
+                continue; // And continue to the next iteration of the loop 
+            }
+            // Each of the children of this GameObject are named 
+            // switch based on the names 
+            switch (tSR.gameObject.name)
+            {
+                case "back": // if the name is "back" 
+                             // Set it to the highest layer to cover the other sprites 
+                    tSR.sortingOrder = sOrd + 2;
+                    break;
+                case "face":  // if the name is "face" 
+                default:      //  or if it's anything else 
+                              // Set it to the middle layer to be above the background           
+                    tSR.sortingOrder = sOrd + 1;
+                    break;
+            }
+        }
+    }
+
+
+    public bool faceUp
+    {
+        get
+        {
+            return (!back.activeSelf);
+        }
+
+        set
+        {
+            back.SetActive(!value);
+        }
+    }
+
+    virtual public void OnMouseUpAsButton()
+    {
+        print(name); // When clicked, this outputs the card name 
+    }
 } // class Card
 
 [System.Serializable]
@@ -50,6 +101,5 @@ public class Decorator{
 public class CardDefinition{
 	public string	face;	//sprite to use for face cart
 	public int		rank;	// value from 1-13 (Ace-King)
-	public List<Decorator>	
-					pips = new List<Decorator>();  // Pips Used
+	public List<Decorator> pips = new List<Decorator>();  // Pips Used
 }
